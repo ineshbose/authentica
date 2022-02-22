c6c2cfc60a18c6ca526fcfad090b0bb86c5815e8
Inesh Bose <2504266b@student.gla.ac.uk>
2022-02-22 10:56:02 +0000

    Auth api added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,20 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
+import { SessionProvider } from 'next-auth/react';
 import { CssBaseline, GeistProvider } from '@geist-ui/core';
 import client from '../graphql/apollo-client';
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ApolloProvider client={client}>
-      <GeistProvider>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </GeistProvider>
-    </ApolloProvider>
+    <SessionProvider session={session}>
+      <ApolloProvider client={client}>
+        <GeistProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </GeistProvider>
+      </ApolloProvider>
+    </SessionProvider>
   );
 }
 
---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export default NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        const res = await fetch('/api', {
+          method: 'POST',
+          body: JSON.stringify(credentials),
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        const user = await res.json();
+        return res.ok && user ? user : null;
+      },
+    }),
+  ],
+});
